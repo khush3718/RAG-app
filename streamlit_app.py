2496ee2d21f5bf60bfda6cb9b5e9856f567c4941
Khush Shah <khushshah51@gmail.com>
2024-11-20 15:44:25 +0530

    initialized streamlit ui for the app

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="RAG Application",page_icon="🔗")
+
+st.title("RAG application")
+st.write("This application allows you to input text or documents, ask questions, and get responses based on your RAG model.")
+
+
